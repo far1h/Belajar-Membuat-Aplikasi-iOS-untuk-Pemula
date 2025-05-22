846af5738952692c8333b1d73410684cb36262d8
far1h <farihmuhammad45@gmail.com>
2025-05-22 21:50:41 +0200

    Initial Commit

---
+++ Belajar Membuat Aplikasi iOS untuk Pemula/Belajar_Membuat_Aplikasi_iOS_untuk_PemulaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Belajar_Membuat_Aplikasi_iOS_untuk_PemulaApp.swift
+//  Belajar Membuat Aplikasi iOS untuk Pemula
+//
+//  Created by Farih Muhammad on 22/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct Belajar_Membuat_Aplikasi_iOS_untuk_PemulaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
