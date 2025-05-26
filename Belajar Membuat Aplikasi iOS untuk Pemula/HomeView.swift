de3b7bde5bcade94f5d11e57f03ccc4dcc48cbc5
far1h <farihmuhammad45@gmail.com>
2025-05-26 07:14:26 +0100

    add image

--- Belajar Membuat Aplikasi iOS untuk Pemula/HomeView.swift
+++ Belajar Membuat Aplikasi iOS untuk Pemula/HomeView.swift
@@ -20,11 +20,19 @@ struct HomeView: View {
                 } else {
                     List(viewModel.doaList.prefix(100)) { doa in
                         NavigationLink(destination: DetailView(doa: doa)) {
-                            VStack(alignment: .leading) {
-                                Text(doa.doa).font(.headline)
-                                if let verse = doa.ayat {
-                                    Text("“\(verse)”").font(.subheadline).lineLimit(1)
+                            HStack(alignment: .center, spacing: 10) {
+                                Image(systemName: "book")
+                                    .resizable()
+                                    .frame(width: 20, height: 20)
+                                    .foregroundColor(.blue)
+                                    .padding(4)
+                                VStack(alignment: .leading) {
+                                    Text(doa.doa).font(.headline)
+                                    if let verse = doa.ayat {
+                                        Text("“\(verse)”").font(.subheadline).lineLimit(1)
+                                    }
                                 }
+                                .padding(.vertical, 4)
                             }
                         }
                     }
